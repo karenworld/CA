2af312ae3b2f0ebc0656ee367d90a692f59b0d74
karenworld <karenworld@users.noreply.github.com>
2016-08-04 17:14:53 +0800

    Add files via upload

---
+++ UNOgame.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Karen Goh Seow Hui
+ */
+public class UNOgame {
+
+    private String gameID;
+
+    private ArrayList<Players> players;
+
+    private String gameStatus; //={"waiting","started","ended"};
+
+    private Deck deck;
+
+    private LinkedList<UNOCard> discardPile;
+
+    public UNOgame() {
+
+        deck = new Deck();
+
+        players = new ArrayList<Players>();
+
+        discardPile = new LinkedList<UNOCard>();
+
+    }
+
+    public UNOgame(String id, ArrayList<Players> player, String status, Deck deck, LinkedList<UNOCard> pile) {
+
+        this.gameID = id;
+
+        this.players = player;
+
+        this.gameStatus = status;
+
+        this.deck = deck;
+
+        this.discardPile = pile;
+
+    }
+
+    public Deck getDeck() {
+
+        return deck;
+
+    }
+
+    public LinkedList<UNOCard> getDiscardPile() {
+
+        return discardPile;
+
+    }
+
+    public List<Players> getPlayers() {
+
+        return players;
+
+    }
+
+    public void createNewgame() {
+
+        deck.createDeck();
+
+    }
+
+    public void addPlayer(Players p) {
+
+// this.players=new ArrayList<Players>();
+        players.add(p);
+
+    }
+
+    public UNOCard takecardfromdeck() {
+
+        Deck deck = new Deck();
+
+        UNOCard c = deck.takeCard();
+
+        return c;
+
+    }
+
+    public void discard(UNOCard card, Players p) {
+
+// Players p=new Players();
+        LinkedList<UNOCard> a = p.getHands();
+
+        discardPile.addFirst(card);
+
+        a.remove(card);
+
+    }
+
+// public UUOCard tcardfrompile(){
+// 
+// }
+    public String changeStatus(String status) {
+
+        gameStatus = status;
+
+        return status;
+
+    }
+
+    public void deal(Players p) {
+
+        for (int i = 0; i < 7; i++) {
+
+            p.addCard(deck);
+
+        }
+
+    }
+
+}
