6658f4a2fe2027de3b3963f6e1a66c1de29e5264
karenworld <karenworld@users.noreply.github.com>
2016-08-04 17:14:10 +0800

    Add files via upload

---
+++ Players.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ *
+ * @author Karen Goh Seow Hui
+ */
+public class Players {
+    private String name;
+    LinkedList<UNOCard>hands=new LinkedList<UNOCard>();
+    Deck deck=new Deck();
+    
+    public Players(){
+        super();
+    }
+    public Players(String name){
+        this.name=name;
+    }
+    public LinkedList<UNOCard> getHands(){
+        return hands;
+    } 
+    public void setName(String name){
+        this.name=name;
+    }
+     
+    public LinkedList<UNOCard> addCard(Deck deck){
+        
+       
+      //  Random rand =new Random();
+      //  int n=rand.nextInt();
+        hands.add(deck.takeCard());
+        return hands;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+    public LinkedList<UNOCard> removeCard(){
+        Random rand=new Random();
+        int n=rand.nextInt(hands.size());
+        hands.remove(n);
+        return hands;
+    }
+    
+        
+    
+}
